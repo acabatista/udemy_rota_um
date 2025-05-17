758b51b16a478c4f775e149f20273601951c9c77
Camila Batista Costa <143089903+acabatista@users.noreply.github.com>
2025-05-17 12:46:20 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express"); // importando o express
+const app = express();
+
+app.get("/",function(req,res){
+    res.send("Bem vindo ao guia do programador")
+});
+
+
+app.get("/blog",function(req, res){
+    res.send("Bem vindo ao meu blog!");
+
+})
+
+app.get("/canal/youtube",function(req,res){
+    res.send("<h1>Bem vindo ao meu canal!</h1>");
+})
+
+app.get("/ola",function(req,res){
+    res.send("<h1>Ola</h1>");
+});
+
+app.listen(8080,function(erro){
+    if(erro){
+        console.log("Ocorreu um erro!");
+
+    }else{
+        console.log("Servidor iniciado com sucesso!");
+    }
+})
+
+    
